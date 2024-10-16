21297ade933057aa46625ac582268e3ac52cc6e5
Jan Sokołowski <67105352+franekjanek52@users.noreply.github.com>
2024-10-17 01:42:44 +0200

    added license

--- Pm02C_Analizator.py
+++ Pm02C_Analizator.py
@@ -1,3 +1,25 @@
+# MIT License
+
+# Copyright (c) 2024 Jan Sokołowski
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+
+# SOFTWARE.
 #================================= Biblioteki ====================================#
 
 import re
@@ -528,13 +550,8 @@ window.close()  # Poza główną pętlą = zamknij okno
 #                                     | -|- |                                     #
 #                                    /   |   \                                    #
 #                                    ---- \___|                                   #
-#=====================================Notatki=====================================#
+#===============================Jan Sokołowski 2024===============================#
 '''
--dodać informacje o poprawnosci przyjętych danych : zgodnosc dlugosci
--jeden przycisk zamiast start stop
--opcja włączenia ciągłego odbierania danych
--poprawic otwieranie danych 
-
 Keys:
 
 -CANVAS-        -FORMAT-            -DTAB1-
